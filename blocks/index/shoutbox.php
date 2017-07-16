a2220b2e6068d5540424fc08c855dd28006f8dbd
Wunax <badsoftpro@gmail.com>
2017-07-16 17:02:20 +0200

    fix css

--- blocks/index/shoutbox.php
+++ blocks/index/shoutbox.php
@@ -39,7 +39,11 @@ $HTMLOUT.= "
 <li>{$refreshbutton}</li>
 </ul>
 </div>
-<input type='text' name='shbox_text' placeholder='Envoyer un message...'>
+<form class="form">
+    <div class="form-item">
+        <input type='text' name='shbox_text' placeholder='Envoyer un message...'>
+    </div>
+</form>
 <span class='input-group-btn'>
 <input class='btn btn-primary' type='submit' value='{$lang['index_shoutbox_send']}' />
 <input type='hidden' name='sent' value='yes' />
